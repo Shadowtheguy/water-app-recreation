8d3e4618ac807b3f17c2d4f0c79ed45cd2169fb7
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-22 20:56:40 -0500

    added variables for all the text translations

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-var 
+//Variables for English and Spanish switches
+var mainTitleEn = "Water Conservation Tips";
+var mainTitleEs = "Consejos Para la Conservación del Agua";
+
+var mainDescriptionEn = "It's important that we all do our part to use less water. Click through this app for tips and conservation ideas.";
+var mainDescriptionEs = "Es importante que todos contribuyamos a usar menos agua. Explora esta aplicación para obtener consejos e ideas de conservación.";
+
+var nextButtonEn = "Next";
+var nextButtonEs = "Continuar";
+
+var bottleLine1En = "Instead of buying diposable water bottled, use a refillable bottle. One use water bottles fill our landfills and trap valuable water.";
+var bottleLine1Es = "En lugar de comprar agua embotellada desechable, usa una botella rellenable. Las botellas de agua de un solo uso llenan los vertederos y retienen agua valiosa.";
+
+var bottleLine2En = "Choose the correct water bottle. Then click the faucet to fill.";
+var bottleLine2Es = "Elige la botella de agua correcta. Luego, haz clic en el grifo para llenarla.";
+
+var showerDescriptionEn = "Another way to conserve water is to take shorter showers. Try Setting a timer to five minutes!";
+var showerDescriptionEs = "Otra forma de ahorrar agua es ducharse más corto. ¡Prueba a programar un temporizador de cinco minutos!";
+
+var homeButtonEn = "Home";
+var homeButtonEs = "Hogar";
