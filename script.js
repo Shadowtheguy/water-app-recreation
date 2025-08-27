2d2fdb6659eb61890f9eb44ddf73866d50608e07
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-26 20:26:48 -0500

    Finally fixed the languages

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
+//Testing
+var language = localStorage.getItem("language");
+
 //Variables for English and Spanish switches
-console.log("hello");
 
 var mainTitleEn = "Water Conservation Tips";
 var mainTitleEs = "Consejos Para la Conservación del Agua";
 
-var mainDescriptionEn = "It's important that we all do our part to use less water. Click through this app for tips and conservation ideas.";
-var mainDescriptionEs = "Es importante que todos contribuyamos a usar menos agua. Explora esta aplicación para obtener consejos e ideas de conservación.";
+var mainDescriptionEn =
+  "It's important that we all do our part to use less water. Click through this app for tips and conservation ideas.";
+var mainDescriptionEs =
+  "Es importante que todos contribuyamos a usar menos agua. Explora esta aplicación para obtener consejos e ideas de conservación.";
 
 var nextButtonEn = "Next";
 var nextButtonEs = "Continuar";
 
-var bottleLine1En = "Instead of buying diposable water bottled, use a refillable bottle. One use water bottles fill our landfills and trap valuable water.";
-var bottleLine1Es = "En lugar de comprar agua embotellada desechable, usa una botella rellenable. Las botellas de agua de un solo uso llenan los vertederos y retienen agua valiosa.";
+var bottleLine1En =
+  "Instead of buying diposable water bottled, use a refillable bottle. One use water bottles fill our landfills and trap valuable water.";
+var bottleLine1Es =
+  "En lugar de comprar agua embotellada desechable, usa una botella rellenable. Las botellas de agua de un solo uso llenan los vertederos y retienen agua valiosa.";
 
-var bottleLine2En = "Choose the correct water bottle. Then click the faucet to fill.";
-var bottleLine2Es = "Elige la botella de agua correcta. Luego, haz clic en el grifo para llenarla.";
+var bottleLine2En =
+  "Choose the correct water bottle. Then click the faucet to fill.";
+var bottleLine2Es =
+  "Elige la botella de agua correcta. Luego, haz clic en el grifo para llenarla.";
 
-var showerDescriptionEn = "Another way to conserve water is to take shorter showers. Try Setting a timer to five minutes!";
-var showerDescriptionEs = "Otra forma de ahorrar agua es ducharse más corto. ¡Prueba a programar un temporizador de cinco minutos!";
+var showerDescriptionEn =
+  "Another way to conserve water is to take shorter showers. Try Setting a timer to five minutes!";
+var showerDescriptionEs =
+  "Otra forma de ahorrar agua es ducharse más corto. ¡Prueba a programar un temporizador de cinco minutos!";
 
 var timerInstructionsEn = "Click on the timer.";
 var timerInstructionsEs = "Haga clic en el temporizador.";
@@ -31,8 +41,24 @@ var bottleMoved = false;
 //Actual Functions
 //! Code works now, just not across pages
 
+setLanguage()
+
 //English Translation
-onEvent("buttonEn", "click", function() {
+onEvent("buttonEn", "click", function () {
+  localStorage.setItem("language", "english");
+  language = localStorage.getItem("language");
+  setLanguage();
+});
+
+//Spanish Translation
+onEvent("buttonEs", "click", function () {
+  localStorage.setItem("language", "spanish");
+  language = localStorage.getItem("language");
+  setLanguage();
+});
+
+function setLanguage() {
+  if (language === "english") {
     //Main Page
     setText("mainPageTitle", mainTitleEn);
     setText("mainPageDesc", mainDescriptionEn);
@@ -45,10 +71,8 @@ onEvent("buttonEn", "click", function() {
     setText("timerPageDesc", showerDescriptionEn);
     setText("timerInstructions", timerInstructionsEn);
     setText("homeButton", homeButtonEn);
-})
-
-//Spanish Translation
-onEvent("buttonEs", "click", function() {
+  } else {
+    console.log(language, "this is the language");
     //Main Page
     setText("mainPageTitle", mainTitleEs);
     setText("mainPageDesc", mainDescriptionEs);
@@ -61,39 +85,42 @@ onEvent("buttonEs", "click", function() {
     setText("timerPageDesc", showerDescriptionEs);
     setText("timerInstructions", timerInstructionsEs);
     setText("homeButton", homeButtonEs);
-})
+  }
+}
 
 //Image Interactions
 
 //Bottle Image Interactions
-onEvent("correctBottle", "click", function() {
-    imageElement = document.getElementById('correctBottle');
-    imageElement.src = 'images/white.png';
+onEvent("correctBottle", "click", function () {
+  imageElement = document.getElementById("correctBottle");
+  imageElement.src = "images/white.png";
 
-    imageElement = document.getElementById('correctBottleMoved');
-    imageElement.src = 'images/waterBottleGlass.png';
+  imageElement = document.getElementById("correctBottleMoved");
+  imageElement.src = "images/waterBottleGlass.png";
 
-    bottleMoved = true;
-})
+  bottleMoved = true;
+});
 
-if(bottleMoved = true) { 
-    onEvent("correctBottleMoved", "click", function() {
-    imageElement = document.getElementById('correctBottleMoved');
-    imageElement.src = 'images/waterBottleFilled.png';
-    })
+if ((bottleMoved = true)) {
+  onEvent("correctBottleMoved", "click", function () {
+    imageElement = document.getElementById("correctBottleMoved");
+    imageElement.src = "images/waterBottleFilled.png";
+  });
 }
 
-onEvent("incorrectBottle", "click", function() {
-    imageElement = document.getElementById('incorrectBottle');
-    imageElement.src = 'images/waterBottleNo.png';
-})
+onEvent("incorrectBottle", "click", function () {
+  imageElement = document.getElementById("incorrectBottle");
+  if(imageElement) {
+      imageElement.src = "images/waterBottleNo.png";
+  }
+});
 
 //Shower Image Interactions
 
-onEvent("timer", "click", function(){
-    imageElement = document.getElementById('timer');
-    imageElement.src = 'images/timerEnd.png';
+onEvent("timer", "click", function () {
+  imageElement = document.getElementById("timer");
+  imageElement.src = "images/timerEnd.png";
 
-    imageElement = document.getElementById('showerCurtain');
-    imageElement.src = 'images/duck.png';
-})
+  imageElement = document.getElementById("showerCurtain");
+  imageElement.src = "images/duck.png";
+});
